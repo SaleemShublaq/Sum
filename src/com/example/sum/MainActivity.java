a6de4318d829ad259dd74d7f41ac58b41aa8095b
SaleemShublaq <eng.saleemshublaq@live.com>
2014-03-16 18:07:36 +0200

    aa

---
+++ src/com/example/sum/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.sum;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button b = (Button)findViewById(R.id.button1);
+		b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent anIntent = new Intent();
+				
+				anIntent.setAction("saleem");
+				anIntent.putExtra("key", "saleem");
+				startActivity(anIntent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
